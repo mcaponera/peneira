4a49a3429127c88256a851378ece935bc648a368
Marina Caponera <m.caponera@gmail.com>
2025-01-18 23:29:49 -0300

    :beers: começa a lógica do spider

---
+++ peneira/peneira/spiders/peneirao_spider.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import scrapy
+
+
+class PeneiraoSpider(scrapy.Spider):
+    name = 'peneira'
+    start_urls = [
+            "https://portaldegovernanca.cbf.com.br/documentos-da-partida"
+        ]
+    
+    def parse(self, response):
+        years_ = response.xpath("//select[@id='ano']/option/text()").extract()
+        years = [int(year) for year in years_ if year.isdigit()]
+
+        for year in years:
+            next_url = f"https://portaldegovernanca.cbf.com.br/documentos-da-partida/campeonatos/{year}"
+            yield scrapy.Request(
+                url=next_url,
+                callback=self.parse_year,
+                meta={'year': year}
+            )
